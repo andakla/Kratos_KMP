32922bcc4681d8995d54a1f99d187f382061566a
nhat.phan <nhat.phan@logigear.com>
2020-06-23 17:42:29 +0700

    1

--- py_sources/utilities/utilities.py
+++ py_sources/utilities/utilities.py
@@ -1,5 +1,4 @@
 import json
-from jsonpath_ng import jsonpath, parse
 
 def compare_json(self, json_src, json_des):
     str_src = json.dumps(json_src, sort_keys=True)
@@ -8,13 +7,13 @@ def compare_json(self, json_src, json_des):
     str_des = json.dumps(js_data, sort_keys=True)
     return (str_src==str_des)
 
-def compare_json_by_node_path(self, json_src, path):
-    lst_value =refix_path(path)
-    jsonpath_expression = parse(lst_value[0])
-    for match in jsonpath_expression.find(json_src):
-        if match.value == lst_value[1]:
-            return True
-    return False
+# def compare_json_by_node_path(self, json_src, path):
+#     lst_value =refix_path(path)
+#     jsonpath_expression = parse(lst_value[0])
+#     for match in jsonpath_expression.find(json_src):
+#         if match.value == lst_value[1]:
+#             return True
+#     return False
 
 def refix_path(self,path):
     list_arg=path.split(":")
